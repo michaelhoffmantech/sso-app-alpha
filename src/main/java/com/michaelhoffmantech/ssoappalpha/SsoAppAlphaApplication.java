23431ba8399f9b1f09f094fab81c7510ef5625a5
Michael Hoffman <mike@michaelhoffmaninc.com>
2018-08-04 20:41:18 -0500

    Initial commit

---
+++ src/main/java/com/michaelhoffmantech/ssoappalpha/SsoAppAlphaApplication.java
@@ -0,0 +1,12 @@
+package com.michaelhoffmantech.ssoappalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoAppAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoAppAlphaApplication.class, args);
+	}
+}
